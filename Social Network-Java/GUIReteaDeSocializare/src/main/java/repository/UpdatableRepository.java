9c9bc256388beb517602a2b8b4d705019c964fc5
dariamarian <daria.marian@stud.ubbcluj.ro>
2024-07-14 13:41:10 +0300

    refactoring

--- Social Network-Java/GUIReteaDeSocializare/src/main/java/repository/UpdatableRepository.java
+++
@@ -1,8 +0,0 @@
-package repository;
-
-import domain.Entity;
-import exceptions.RepoException;
-
-public interface UpdatableRepository<E extends Entity<Long>> {
-    void update(E obj) throws RepoException;
-}
